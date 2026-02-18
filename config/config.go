95250eb73bc7a2004daabf4bb4c36ed2906f557c
Pasupuleti-Meghana <pasupuletimeghana21@gmail.com>
2026-02-18 22:52:37 +0530

    Parsed config file

---
+++ config/config.go
@@ -0,0 +1,42 @@
+package config
+
+type HTTPServer struct {
+	Address string `yaml:"address" env-required:"true"`
+}
+
+type Config struct {
+	Env string `yaml:"env" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer 
+}
+
+func MustLoad() *Config {
+	var configpath string 
+	configpath = os.Getenv("CONFIG_PATH")
+	fmt.Println("config path:", configpath)
+
+	if configpath == "" {
+		flags := flag.String("config", "", "path to config file")
+		flag.Parse()
+
+		configpath = *flags
+
+		
+		if configpath == "" {
+			log.Fatal("config path is not set")
+		}
+	}
+
+	if _, err := os.Stat(configpath) {
+		log.Fatalf("config file does not exist: %s", configpath)
+	}
+
+	var cfg Config 
+
+	err := cleanenv.ReadConfig(configpath, &cfg)
+	if err != nil {
+		log.Fatalf("failed to read config file: %s", err.Error())
+	}
+
+	return &cfg
+}
